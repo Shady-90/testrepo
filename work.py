aa9dac67c769593a8488e95119b0a1daf60e4b61
Shady-90 <128419254+Shady-90@users.noreply.github.com>
2023-03-29 11:02:21 -0400

    Add files via upload

---
+++ work.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio 
+# 
+
+# **Shaddai Lafleur**
+# 
+# Data analyst
+
+# *I am interested in data science because i love work with a lot a data and help to make decision*
+
+# ### Addition
+
+# In[14]:
+
+
+a = 26
+b = 27
+c = a + b
+print( "le resultat de l'addition", a ," et" ,b," est:", c)
+
+
+# list :
+# - one
+# - two
+# 
+# [Catalog](https://dataplatform.cloud.ibm.com/gallery)
+# 
+# ![Business-intelligence.png](attachment:Business-intelligence.png)
+# 
+# 
+# | Stretch/Untouched | ProbDistribution | Accuracy |
+# | --- | --- | --- |
+# | Stretched | Gaussian | .843 |
+# 
+
+# In[ ]:
+
+
+
+
